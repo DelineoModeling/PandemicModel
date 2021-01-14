00575ba1ca814c12945951cd4902e3d087728761
marcelomorales <lmoral10@jhu.edu>
2021-01-14 11:19:26 -0500

    styling team

--- model_app/frontend/src/containers/Team.js
+++ model_app/frontend/src/containers/Team.js
@@ -12,6 +12,8 @@ import Grid from '@material-ui/core/Grid';
 import { styled } from "@material-ui/core/styles";
 import { spacing } from "@material-ui/system";
 import MuiButton from "@material-ui/core/Button";
+import Typography from '@material-ui/core/Typography';
+
 
 
 import './Team.css';
@@ -60,51 +62,37 @@ function Team(props){
     <div className = 'CardBackground'>
 
       <div className="MainHeader">
-        <h2>Meet The Team</h2>
+        <h2 className="heading">Meet The Team</h2>
+      </div>        
+
+      <div className = 'CardGreenBackground1'>
+        <h3 align="left">Research Supervisor</h3>
+        <SupGridList >
+        </SupGridList>
       </div>
 
       
-    
-  
 
-      <div className = "Supervisor" align="center" >
-        <h3 align="center">Research Supervisor</h3>
-
-       
-
-        <SupGridList   container
-  spacing={0}
-  direction="column"
-  alignItems="center"
-  justify="center"
-  style={{ minHeight: '100vh' }}  ></SupGridList>
-
-
-
-     
-
-      </div>
-
-      <div className = 'TeamFilter'>
-
+      <div className="about-section">
       
-      <Button mt={2} variant="contained" color="primary" className='TeamFilter' onClick={() => { console.log('onClick'); }} >All
-        </Button>
-        <Button mt={2}  variant="contained" color="primary" className='TeamFilter' onClick={() => { console.log('onClick'); }} >Full Stack
-        </Button>
-        <Button mt={2} variant="contained" color="primary" className='TeamFilter' >Simulation
-        </Button>
-        <Button mt={2} variant="contained" color="primary" className='TeamFilter' >Machine Learning
-        </Button>
-
-        <Button mt={2} variant="contained" color="primary" className='TeamFilter' >Information Support
-        </Button>
-       
-      </div>
-
-      <div className = 'TeamAbout'>
-        <h3> Team Responsibility Descriptions... </h3>
-      </div>
+                   <Grid container spacing={0}>
+                       <Grid item xs={13}>
+                           <Typography variant="h5" component="h3" gutterBottom>
+                               Team description
+                       </Typography>
+                           <p>
+                               Lorem ipsum dolor sit amet, consectetur adipiscing elit. In arcu ligula, auctor ut dictum ac, malesuada et arcu. Vestibulum ut commodo enim, in pulvinar magna. Mauris est enim, pretium id porta ut, maximus at felis. Donec aliquam, velit a tempus accumsan, lorem metus viverra augue, ac convallis quam enim quis lacus. Aliquam ipsum orci, ullamcorper vel erat in, semper posuere tortor. Vestibulum consequat ante nec mauris iaculis ultrices. Vivamus non sagittis sem. Duis a laoreet ante, in facilisis nisl. Quisque tempor non orci id accumsan. Ut nulla augue, ultricies vel odio sed, porta finibus massa. Nunc auctor ante sit amet mi fermentum accumsan. Phasellus fermentum sed lacus id ornare. Curabitur congue, neque sed porta faucibus, risus orci aliquet ex, id fermentum nisl sapien vel sem. Nunc sodales, elit et hendrerit consectetur, sem mauris tempus ligula, sed faucibus quam libero a nibh. Sed orci ex, imperdiet dignissim urna ac, egestas bibendum tortor.
+                       </p>
+                           <div className='button-container'>
+ 
+                               <Button className="Button" variant="contained" color="inherit" href="#top">
+                                   Learn More
+                       </Button>
+                           </div>
+                       </Grid>
+                      
+                   </Grid>
+               </div>
 
 
 
