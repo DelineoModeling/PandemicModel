6cc86f5908d1e393a8efdfe4ee67188c68d8681c
Louie DiBernardo <justthetechie@gmail.com>
2021-02-23 21:55:13 -0800

    completed all recovery traj tasks. lots of todos!

--- simulation/person.py
+++ simulation/person.py
@@ -1,10 +1,12 @@
+import random
+
 class Person:
 
     # Initialization function, sets all parameters at once.
     # TODO: have some default parameters if we can't set all of them at once, for initializing them with synthpops.
-    def __init__(self, ID, age=0, sex=0, householdLocation=0, householdMembers=[], comorbidities=0, demographicInfo=0, severityRisk=0, currentLocation=0, infectionState=0, incubation=0):
+    def __init__(self, ID, age=0, sex=0, householdLocation=0, householdMembers=[], comorbidities=0, demographicInfo=0, severityRisk=0, currentLocation=0, infectionState=0, incubation=0, infectionTimer=0, peakStateDays=4, maximumInfectionState=-1, recoveryDays = [0, 0, 0, 0]):
         self.setAllParameters(ID, age, sex, householdLocation, householdMembers, comorbidities,
-                              demographicInfo, severityRisk, currentLocation, infectionState, incubation)
+                              demographicInfo, severityRisk, currentLocation, infectionState, incubation, infectionTimer, peakStateDays, maximumInfectionState, recoveryDays)
 
     # Sets all parameters.
     def setAllParameters(self, ID, age=0, sex=0, householdLocation=0, householdMembers=[], comorbidities=0, demographicInfo=0, severityRisk=0, currentLocation=0, infectionState=False, incubation=0):
@@ -49,6 +51,7 @@ class Person:
 
     def setInfectionState(self, state):
         self.infectionState = state
+        self.infectionTimerStart()
 
     def setIncubation(self, incubation):
         self.incubation = incubation
@@ -93,3 +96,118 @@ class Person:
 
     def updateState(self):
         return self.incubation + self.severityRisk
+
+    #TODO Ask about severity risk matrix. Where/where will it be implemented?
+
+
+    #called once infected and each day that passes.
+    #Once infected, infection function should update this value to 1.
+    def infectionTimerStart(self):
+        #once passed 15 days, infection is over.
+        #TODO will change depending on severity level.
+        if self.infectionTimer > 15:
+            self.infectionTimer = 0
+
+        #Otherwise, incremement infectionTimer.
+        self.infectionTimer += 1
+
+    #needs to be called once infected. (and updated each day until 0)
+    #TODO incubation days change based on severityRisk/age?
+    def incubationAssignment(self):
+        #get a random number between 1-3 for incubation days
+        randNum = random.randInt(1, 3)
+        self.incubation = randNum
+
+    #Assign number of days spent at peak state based on severity risk
+    #TODO update according to info support research
+    def assignNumDaysPeakState(self):
+        #peakStateDays ranging from 4-10 days, based on severityRisk
+        if self.severityRisk >= 0 & self.severityRisk <=25:
+            self.peakStateDays = 4
+
+        elif self.severityRisk >= 25 & self.severityRisk <= 50:
+            self.peakStateDays = 6
+
+        elif self.severityRisk >= 50 & self.severityRisk <= 75:
+            self.peakStateDays = 8
+
+        else:
+            self.peakStateDays = 10
+
+    #assigns number of recovery days left.
+    def assignNumRecoveryDays(self):
+        #once they have finished their peak days, we can use the infectionTimer to
+        #calculate how many days they should spend recovering.
+        #Kept split up into the four infection states so the numbers can easily be changed.
+        if self.peakStateDay == 0:
+
+            if self.infectionState == 0: #asymptomatic
+                #14 to 21 days for recovery
+                randNum = random.randInt(14, 21)
+                recoveryDaysToBeDivided = randNum - self.infectionTimer
+
+            elif self.infectionState == 1: #mild
+                # 14 to 21 days for recovery
+                randNum = random.randInt(14, 21)
+                recoveryDaysToBeDivided = randNum - self.infectionTimer
+
+            elif self.infectionState == 2:
+                #21 to 42 days for recovery
+                randNum = random.randInt(21, 42)
+                recoveryDaysToBeDivided = randNum - self.infectionTimer
+
+            else:
+                #crticial
+                randNum = random.randInt(21, 42)
+                recoveryDaysToBeDivided = randNum - self.infectionTimer
+
+        #divide the recoveryDays between the different states
+        recoveryDaysToBeDivided = randNum - self.infectionTimer
+
+        #divide by number of states left.
+        daysPerState = recoveryDaysToBeDivided // (self.infectionState + 1)
+
+        #check to see if any days were left over from round-down division, add to asymptomatic days.
+        extraAsymptomatic = 0
+        if daysPerState * self.infectionState < recoveryDaysToBeDivided:
+            extraAsymptomatic = recoveryDaysToBeDivided - daysPerState * self.infectionState
+
+        #add the recovery days per state into the relevant states left.
+        i = self.infectionState
+        while i >= 0:
+            #if 0, asymptomatic days might need to be added.
+            if i == 0:
+                self.recoveryDays[i] = daysPerState + extraAsymptomatic
+            else:
+                #rest of the states
+                self.recoveryDays[i] = daysPerState
+
+            i -= 1
+
+
+
+
+    def stateUp(self):
+        #once incubation period complete, being moving into next states
+        if self.incubation <= 0:
+        #maximum infection state variable created
+        #TODO check to see where implementation of severity risk matrix will be done and how we will get that info.
+            if self.infectionState < self.maximumInfectionState:
+                #increment infectionState by  1
+                self.infectionState += 1
+
+
+
+    def stateDown(self):
+        #once peakStateDays end, moving down each date
+        #Recovered by day 15
+
+        #patient has few more recoveryDays left in their current infectionState
+        if self.recoveryDays[self.infectionState] != 0:
+            self.recoveryDays[self.infectionState] -= 1
+
+        else:
+            #no more days left in the current infectionState
+            #move down one state
+            self.infectionState -= 1
+            self.recoveryDays[self.infectionState] -= 1
